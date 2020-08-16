9983d5b2f815ea340119eccc45418d7925c857a5
florinmihu31 <53407990+florinmihu31@users.noreply.github.com>
2020-08-16 11:28:11 +0300

    Add SecondDescendantActivity imageView functionality

--- app/src/main/java/com/example/mobylabtema1/Constants.java
+++ app/src/main/java/com/example/mobylabtema1/Constants.java
@@ -3,4 +3,7 @@ package com.example.mobylabtema1;
 public class Constants {
     public static final double LOCATION_X = 44.435065;
     public static final double LOCATION_Y = 26.047774;
+
+    public static final int IMAGE_WIDTH = 400;
+    public static final int IMAGE_HEIGHT = 400;
 }
--- app/src/main/java/com/example/mobylabtema1/SecondDescendantActivity.java
+++ app/src/main/java/com/example/mobylabtema1/SecondDescendantActivity.java
@@ -2,7 +2,15 @@ package com.example.mobylabtema1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import static com.example.mobylabtema1.Constants.IMAGE_WIDTH;
+import static com.example.mobylabtema1.Constants.IMAGE_HEIGHT;
 
 public class SecondDescendantActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class SecondDescendantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_descendant);
+
+        ImageView imageView = findViewById(R.id.image_view);
+        Glide.with(this)
+                .load(R.drawable.happy_turtle)
+                .override(IMAGE_WIDTH, IMAGE_HEIGHT)
+                .into(imageView);
+    }
+
+    public void startFirstDescendantActivity(View view) {
+        Intent firstDescendantIntent = new Intent(this, FirstDescendantActivity.class);
+        startActivity(firstDescendantIntent);
     }
 }
