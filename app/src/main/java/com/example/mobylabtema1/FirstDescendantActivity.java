15f9fac8306c1f391d7ed06bd82238cc8c2541c4
florinmihu31 <53407990+florinmihu31@users.noreply.github.com>
2020-08-16 10:53:36 +0300

    Add FirstDescendantActivity elements and functionality

--- app/src/main/java/com/example/mobylabtema1/FirstDescendantActivity.java
+++ app/src/main/java/com/example/mobylabtema1/FirstDescendantActivity.java
@@ -2,7 +2,9 @@ package com.example.mobylabtema1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstDescendantActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FirstDescendantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_descendant);
+    }
+
+    public void startSecondDescendantActivity(View view) {
+        Intent secondDescendantIntent = new Intent(this, SecondDescendantActivity.class);
+        startActivity(secondDescendantIntent);
     }
 }
